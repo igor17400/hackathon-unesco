9a69786f2b9610dc6f953231f70b4964b1be4c3a
Igor Lima <igorlima1740@gmail.com>
2020-04-23 16:20:11 -0300

    Updates

---
+++ precache-manifest.6dd07c258e3841570d0a63f289fe97aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5784182a43cda06cf1503bca3b052783",
+    "url": "/hackathon-unesco/index.html"
+  },
+  {
+    "revision": "06fd17292351730aef40",
+    "url": "/hackathon-unesco/static/css/2.d4d00375.chunk.css"
+  },
+  {
+    "revision": "a5a2d1361853f37d35fd",
+    "url": "/hackathon-unesco/static/css/main.8ed55c49.chunk.css"
+  },
+  {
+    "revision": "06fd17292351730aef40",
+    "url": "/hackathon-unesco/static/js/2.ea6eb65b.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/hackathon-unesco/static/js/2.ea6eb65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a2d1361853f37d35fd",
+    "url": "/hackathon-unesco/static/js/main.08adc33a.chunk.js"
+  },
+  {
+    "revision": "6400db4bdc0c5367b361",
+    "url": "/hackathon-unesco/static/js/runtime-main.700428b4.js"
+  },
+  {
+    "revision": "b9f62f7a66ba695345043fd6b57b03fe",
+    "url": "/hackathon-unesco/static/media/Mobile.b9f62f7a.png"
+  },
+  {
+    "revision": "28def17ddc4db526b25534398cdb1bbd",
+    "url": "/hackathon-unesco/static/media/meghan.28def17d.jpg"
+  },
+  {
+    "revision": "88f86e0177ef72327dac0827edec8e9e",
+    "url": "/hackathon-unesco/static/media/samantha.88f86e01.jpg"
+  }
+]);
